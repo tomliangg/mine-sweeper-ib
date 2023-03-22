628cb142876e5d25e675ff68705a304a2a5fa7d4
tomliangg <tomliangg@gmail.com>
2023-03-21 21:06:45 -0700

    feat: integrate Board and GameControl component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,90 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+import { settings, GameStatus, Setting } from "./constants";
+import { generateMineSweeperBoard, generateMineLocations } from "./utils";
+import { Board } from "./Board";
+import { GameControl } from "./GameControl";
+import "./App.scss";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  const [setting, setSetting] = useState(settings[0]);
+  const [flags, setFlags] = useState(setting.mineCounts);
+  const [cheatMode, setCheatMode] = useState(false);
+  const [board, setBoard] = useState(
+    generateMineSweeperBoard(
+      setting.boardSize,
+      generateMineLocations(setting.boardSize, setting.mineCounts)
+    )
+  );
+
+  const [status, setStatus] = useState(GameStatus.Playing);
+
+  const customSetting = settings.at(-1) as Setting;
+  const [customRowSize, setCustomRowSize] = useState(
+    customSetting.boardSize[0]
+  );
+  const [customColSize, setCustomColSize] = useState(
+    customSetting.boardSize[1]
+  );
+  const [customMineCounts, setCustomMineCounts] = useState(
+    customSetting.mineCounts
+  );
+
+  useEffect(() => {
+    handleRestartGame();
+  }, [setting.difficulty, customRowSize, customColSize, customMineCounts]);
+
+  const handleRestartGame = () => {
+    setStatus(GameStatus.Playing);
+
+    if (setting.difficulty === "Custom") {
+      setFlags(customMineCounts);
+      const customBoardSize: [number, number] = [customRowSize, customColSize];
+      setBoard(
+        generateMineSweeperBoard(
+          customBoardSize,
+          generateMineLocations(customBoardSize, customMineCounts)
+        )
+      );
+    } else {
+      setFlags(setting.mineCounts);
+      setBoard(
+        generateMineSweeperBoard(
+          setting.boardSize,
+          generateMineLocations(setting.boardSize, setting.mineCounts)
+        )
+      );
+    }
+  };
+
+  return (
+    <div className="App">
+      <GameControl
+        setting={setting}
+        setSetting={setSetting}
+        flags={flags}
+        handleRestartGame={handleRestartGame}
+        customColSize={customColSize}
+        setCustomColSize={setCustomColSize}
+        customRowSize={customRowSize}
+        setCustomRowSize={setCustomRowSize}
+        customMineCounts={customMineCounts}
+        setCustomMineCounts={setCustomMineCounts}
+        setCheatMode={setCheatMode}
+      />
+      <Board
+        board={board}
+        setBoard={setBoard}
+        status={status}
+        setStatus={setStatus}
+        flags={flags}
+        setFlags={setFlags}
+        cheatMode={cheatMode}
+      />
+      {status !== GameStatus.Playing && (
+        <button className={`info ${status}`} onClick={handleRestartGame}>
+          <span>{`you ${status} (click here to play again)`}</span>
+        </button>
+      )}
+    </div>
+  );
 }
-
-export default App;
